b3d1c2eec326d5fb8234999263f3b3353afb9378
krzysilelek <1kczarnecki@gmail.com>
2023-11-09 11:50:31 +0100

    Changed error into sending status
    "Authentication failed!"

--- controllers/AuthorizationController.js
+++ controllers/AuthorizationController.js
@@ -9,9 +9,7 @@ exports.login = async (req, res) => {
   const user = req.user;
   const validPassword = await bcrypt.compare(password, user.password);
 
-  if (!validPassword) {
-    throw new Error("Authetication failed!");
-  }
+  if (!validPassword) return res.sendStatus(402);
 
   const accessToken = createAccessToken(user.id, ACCESS_TOKEN_AGE);
   const refreshToken = createRefreshToken(user.id, REFRESH_TOKEN_AGE);
